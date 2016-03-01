072c5395126200eeae4a2b67d70b075b5646d6fb
sander <s.teunissen@gmail.com>
2016-03-01 16:31:07 +0100

    some changes

--- aio_videotimer.py
+++ aio_videotimer.py
@@ -1,20 +1,18 @@
-import logging
-
 import requests
 from aiohttp import web
 
 
 async def send_to_hub(request):
-    #logging.debug("activating scene {}".format(scene_id))
-    scene_id = request.match_info.get('scene_id','')
+    # logging.debug("activating scene {}".format(scene_id))
+    scene_id = request.match_info.get('scene_id', '')
     requests.get('http://192.168.0.106/api/scenes?sceneid={}'.format(scene_id))
     return web.Response(body=b"succeeded")
 
 
 app = web.Application()
 
-app.router.add_route('GET', '/hub/{scene_id}',send_to_hub)
-app.router.add_static("/static/","static/")
+app.router.add_route('GET', '/hub/{scene_id}', send_to_hub)
+app.router.add_static("/static/", "static/")
 
 if __name__ == "__main__":
     web.run_app(app)
--- videotimer.py
+++ videotimer.py
@@ -1,16 +1,20 @@
-from flask import Flask, redirect
-app = Flask(__name__, static_folder="static")
+from flask import Flask
+
+app = Flask(__name__)
 import requests
+import os
 
 import logging
 
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 @app.route('/hub/<scene_id>')
 def send_to_hub(scene_id):
     logging.debug("activating scene {}".format(scene_id))
     requests.get('http://192.168.0.106/api/scenes?sceneid={}'.format(scene_id))
     return "succeeded"
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
